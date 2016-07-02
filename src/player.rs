f107565d31db0628889a7ce18a0d0128ddf813ee
TheZoq2 <frans.skarman@gmail.com>
2016-07-02 17:05:25 +0200

    Add player file

---
+++ src/player.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+
+#[derive(Eq, PartialEq, Hash)]
+pub enum Keys
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+pub struct PlayerComponent 
+{
+    pressed_keys: HashMap<Keys, bool>,
+}
+
+impl PlayerComponent
+{
+    pub fn new() -> PlayerComponent
+    {
+        PlayerComponent
+        {
+            pressed_keys: HashMap::new(),
+        }
+    }
+
+    pub fn set_key(&mut self, key: Keys, pressed: bool)
+    {
+        self.pressed_keys.insert(key, pressed);
+    }
+    pub fn get_key(&self, key: Keys) -> bool 
+    {
+        match self.pressed_keys.get(&key)
+        {
+            Some(down) => down.clone(),
+            None => false,
+        }
+    }
+}
